69716a8bfa78e594b604aec67e495f648fd88d67
fazila7002 <fazilatunnisa705@gmail.com>
2026-05-05 18:48:51 +0700

    tambahin isi contructor untuk tampilkan data nim dan nama

--- contructorberparameter.cpp
+++ contructorberparameter.cpp
@@ -5,3 +5,9 @@ class Mahasiswa {
 public:
 Mahasiswa(int nim, string nama);
 };
+
+Mahasiswa::Mahasiswa(int nim, string nama){
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout << "Nim : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
